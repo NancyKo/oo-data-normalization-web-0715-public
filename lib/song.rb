849dc99eb5fd273bcaf9375782445fb723736d95
NancyKo <konancys@gmail.com>
2015-08-09 17:01:44 -0400

    Commiting what I have so far

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Song
   # code goes here
+attr_writer :title, :artist
+
+  def title
+    @title
+  end
+
+  def serialize
+    artist_name = File.new(@artist.name.downcase.gsub(" ", "_"), "w")
+    binding.pry
+    
+  end
+
+  def artist 
+    @artist
+  end
+
+
 end
